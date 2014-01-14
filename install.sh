6acb9a47918236a17c963d9ca11f2626a5d62aed
Jon Morehouse <morehousej09@gmail.com>
2014-01-14 02:16:10 -0800

    Rebuild installation script

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# make this script callable from anywhere!
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
 for i in `ls *vimrc`
 do
 
-	ln -f -s `pwd`/$i $HOME/.$i
-
+	ln -f -s $DIR/$i $HOME/.$i
 done
 
 
