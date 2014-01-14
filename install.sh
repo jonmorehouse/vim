648a73e9fdb50464346d47e2983d68fd60f097b3
Jon Morehouse <morehousej09@gmail.com>
2014-01-14 02:03:26 -0800

    Build simplified install script

--- install.sh
+++ install.sh
@@ -1,28 +1,14 @@
-# copy the vimrc to the correct location on the filesystem!
-# make sure that vim-nox is installed before hand
-path=$(pwd)
+#!/bin/bash
+for i in `ls *vimrc`
+do
 
-# make sure that we back up the correct file etc
-if [ -f $HOME/.vimrc ];
-then
-	mv $HOME/.vimrc $HOME/.vimrc.old	
-fi
+	ln -f -s `pwd`/$i $HOME/.$i
 
-# ensure that we dont have a functions.vimrc already in our main path  
-if [ -f $HOME/.functions.vimrc ];
-then
-	mv $HOME/.functions.vimrc $HOME/.functions.vimrc.old
-fi
+done
 
-# symlink the vimrc / vim functions files
-cd $HOME && ln -s $path/vimrc .vimrc
-cd $HOME && ln -s $path/functions.vimrc .functions.vimrc
 
-# only symlink the 
-if [ ! -d $HOME/.vim ]; 
-then
-	cd $HOME && ln -s $path/vim .vim
-fi
+
+
 
 
 
