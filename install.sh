5af5e364773cbe189662a21370266109aed938da
Jon Morehouse <morehousej09@gmail.com>
2014-01-18 19:35:02 -0800

    Update vim install further. Continue trying to debug application

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# export old cc as we need to use the default system compilers for command-t
+export OLD_CC=$CC
+export CC=
+
 # install all submodule dependencies
 #git submodule foreach --recursive git submodule update --init
 
@@ -7,20 +11,21 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
+# loop through all the vim symlinks and properly link them up
 for i in `ls *imrc`
 do
-
 	ln -f -s $DIR/$i $HOME/.$i
 done
 
 # install command-t
 cd bundle/command-t 
-make
+make clean && make clean
 vim -c 'silent so % | d | d | quitall!' command-t.vba
 cd ../../ruby/command-t
 ruby extconf.rb
-make
+make clean && make
 cd $DIR
 
-
+# clean up after ourselves here
+export CC=$OLD_CC
 
