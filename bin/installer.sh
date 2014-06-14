7cd25440f7d6c1a1e078372d474e28e27b9797fc
Jon Morehouse <morehousej09@gmail.com>
2014-06-14 00:43:11 -0700

    Remove command-t dependency since its not used much anymore. Update install script accordingly.

--- bin/installer.sh
+++ bin/installer.sh
@@ -1,22 +1,15 @@
 if [ $OS = "mac" ]; then
-    brew install vim
+    brew install vim --with-python3
 else
     # install ruby dependencies
     sudo apt-get install -y ruby ruby-dev vim
 fi
 
 # clone repository to the correct place
-git clone --recursive git@github.com:jonmorehouse/vim ~/.vim
+if [[ ! -d $HOME/.vim ]];then
+  git clone --recursive git@github.com:jonmorehouse/vim ~/.vim
+fi
 
 # now install the dependencies
 vim +PluginInstall +qall
-
-# now install the command-t plugin
-cd ~/.vim/bundle/Command-T && make
-
-# source vimball
-vim -c 'so %' -c 'q' ~/.vim/bundle/Command-T/command-t.vba
-
-# install command-t plugin
-cd ~/.vim/bundle/Command-T/ruby && ruby extconf.rb && make
 
